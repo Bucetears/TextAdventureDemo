76d1c109792502dac6ceb4400df9cfd00ca12576
Cecelia <CeceliaPendell@gmail.com>
2024-10-04 16:37:19 -0600

    Player class created

---
+++ player.hpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <iostream>
+using namespace std;
+
+class Player
+{
+    public:
+    int affectionValue = 5;
+    int snmValue = 5;
+
+};
+
+
+#endif
