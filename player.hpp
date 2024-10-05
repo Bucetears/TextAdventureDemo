fb1d86d9b1843444e63bcea4f439f27b5406348c
Cecelia <CeceliaPendell@gmail.com>
2024-10-04 18:40:06 -0600

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -10,8 +10,6 @@ class PLAYER
     public:
     int affectionValue = 5;
     int snmValue = 5;
-    void alterValues(vector<int> values);
-
     void alterValues(vector<int> values)
     {
         affectionValue += values[0];
